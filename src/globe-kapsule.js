4b115b38cc71a58b02dbd651a442a1608cf3afec
Fielding Johnston <fielding@justfielding.com>
2022-03-08 19:54:34 -0600

    Remove label layer

--- src/globe-kapsule.js
+++ src/globe-kapsule.js
@@ -1,16 +1,12 @@
-import {
-  Group,
-  Vector2,
-  Vector3
-} from 'three';
+import { Group, Vector2, Vector3 } from 'three';
 
 const THREE = window.THREE
   ? window.THREE // Prefer consumption from global THREE, if exists
   : {
-    Group,
-    Vector2,
-    Vector3
-  };
+      Group,
+      Vector2,
+      Vector3,
+    };
 
 import Kapsule from 'kapsule';
 import TWEEN from '@tweenjs/tween.js';
@@ -27,7 +23,7 @@ import PolygonsLayerKapsule from './layers/polygons';
 import HexedPolygonsLayerKapsule from './layers/hexedPolygons';
 import PathsLayerKapsule from './layers/paths';
 import TilesLayerKapsule from './layers/tiles';
-import LabelsLayerKapsule from './layers/labels';
+// import LabelsLayerKapsule from './layers/labels';
 import RingsLayerKapsule from './layers/rings';
 import HtmlElementsLayerKapsule from './layers/htmlElements';
 import ObjectsLayerKapsule from './layers/objects';
@@ -44,191 +40,204 @@ const layers = [
   'hexedPolygonsLayer',
   'pathsLayer',
   'tilesLayer',
-  'labelsLayer',
+  // 'labelsLayer',
   'ringsLayer',
   'htmlElementsLayer',
   'objectsLayer',
-  'customLayer'
+  'customLayer',
 ];
 
 // Expose config from layers
 const bindGlobeLayer = linkKapsule('globeLayer', GlobeLayerKapsule);
-const linkedGlobeLayerProps = Object.assign(...[
-  'globeImageUrl',
-  'bumpImageUrl',
-  'showGlobe',
-  'showGraticules',
-  'showAtmosphere',
-  'atmosphereColor',
-  'atmosphereAltitude'
-].map(p => ({ [p]: bindGlobeLayer.linkProp(p)})));
+const linkedGlobeLayerProps = Object.assign(
+  ...[
+    'globeImageUrl',
+    'bumpImageUrl',
+    'showGlobe',
+    'showGraticules',
+    'showAtmosphere',
+    'atmosphereColor',
+    'atmosphereAltitude',
+  ].map((p) => ({ [p]: bindGlobeLayer.linkProp(p) })),
+);
 
-const linkedGlobeLayerMethods = Object.assign(...[
-  'globeMaterial'
-].map(p => ({ [p]: bindGlobeLayer.linkMethod(p)})))
+const linkedGlobeLayerMethods = Object.assign(...['globeMaterial'].map((p) => ({ [p]: bindGlobeLayer.linkMethod(p) })));
 
 const bindPointsLayer = linkKapsule('pointsLayer', PointsLayerKapsule);
-const linkedPointsLayerProps = Object.assign(...[
-  'pointsData',
-  'pointLat',
-  'pointLng',
-  'pointColor',
-  'pointAltitude',
-  'pointRadius',
-  'pointResolution',
-  'pointsMerge',
-  'pointsTransitionDuration'
-].map(p => ({ [p]: bindPointsLayer.linkProp(p)})));
+const linkedPointsLayerProps = Object.assign(
+  ...[
+    'pointsData',
+    'pointLat',
+    'pointLng',
+    'pointColor',
+    'pointAltitude',
+    'pointRadius',
+    'pointResolution',
+    'pointsMerge',
+    'pointsTransitionDuration',
+  ].map((p) => ({ [p]: bindPointsLayer.linkProp(p) })),
+);
 
 const bindArcsLayer = linkKapsule('arcsLayer', ArcsLayerKapsule);
-const linkedArcsLayerProps = Object.assign(...[
-  'arcsData',
-  'arcStartLat',
-  'arcStartLng',
-  'arcEndLat',
-  'arcEndLng',
-  'arcColor',
-  'arcAltitude',
-  'arcAltitudeAutoScale',
-  'arcStroke',
-  'arcCurveResolution',
-  'arcCircularResolution',
-  'arcDashLength',
-  'arcDashGap',
-  'arcDashInitialGap',
-  'arcDashAnimateTime',
-  'arcsTransitionDuration'
-].map(p => ({ [p]: bindArcsLayer.linkProp(p)})));
+const linkedArcsLayerProps = Object.assign(
+  ...[
+    'arcsData',
+    'arcStartLat',
+    'arcStartLng',
+    'arcEndLat',
+    'arcEndLng',
+    'arcColor',
+    'arcAltitude',
+    'arcAltitudeAutoScale',
+    'arcStroke',
+    'arcCurveResolution',
+    'arcCircularResolution',
+    'arcDashLength',
+    'arcDashGap',
+    'arcDashInitialGap',
+    'arcDashAnimateTime',
+    'arcsTransitionDuration',
+  ].map((p) => ({ [p]: bindArcsLayer.linkProp(p) })),
+);
 
 const bindHexBinLayer = linkKapsule('hexBinLayer', HexBinLayerKapsule);
-const linkedHexBinLayerProps = Object.assign(...[
-  'hexBinPointsData',
-  'hexBinPointLat',
-  'hexBinPointLng',
-  'hexBinPointWeight',
-  'hexBinResolution',
-  'hexMargin',
-  'hexTopCurvatureResolution',
-  'hexTopColor',
-  'hexSideColor',
-  'hexAltitude',
-  'hexBinMerge',
-  'hexTransitionDuration'
-].map(p => ({ [p]: bindHexBinLayer.linkProp(p)})));
+const linkedHexBinLayerProps = Object.assign(
+  ...[
+    'hexBinPointsData',
+    'hexBinPointLat',
+    'hexBinPointLng',
+    'hexBinPointWeight',
+    'hexBinResolution',
+    'hexMargin',
+    'hexTopCurvatureResolution',
+    'hexTopColor',
+    'hexSideColor',
+    'hexAltitude',
+    'hexBinMerge',
+    'hexTransitionDuration',
+  ].map((p) => ({ [p]: bindHexBinLayer.linkProp(p) })),
+);
 
 const bindHexedPolygonsLayer = linkKapsule('hexedPolygonsLayer', HexedPolygonsLayerKapsule);
-const linkedHexedPolygonsLayerProps = Object.assign(...[
-  'hexPolygonsData',
-  'hexPolygonGeoJsonGeometry',
-  'hexPolygonColor',
-  'hexPolygonAltitude',
-  'hexPolygonResolution',
-  'hexPolygonMargin',
-  'hexPolygonCurvatureResolution',
-  'hexPolygonsTransitionDuration'
-].map(p => ({ [p]: bindHexedPolygonsLayer.linkProp(p)})));
+const linkedHexedPolygonsLayerProps = Object.assign(
+  ...[
+    'hexPolygonsData',
+    'hexPolygonGeoJsonGeometry',
+    'hexPolygonColor',
+    'hexPolygonAltitude',
+    'hexPolygonResolution',
+    'hexPolygonMargin',
+    'hexPolygonCurvatureResolution',
+    'hexPolygonsTransitionDuration',
+  ].map((p) => ({ [p]: bindHexedPolygonsLayer.linkProp(p) })),
+);
 
 const bindPolygonsLayer = linkKapsule('polygonsLayer', PolygonsLayerKapsule);
-const linkedPolygonsLayerProps = Object.assign(...[
-  'polygonsData',
-  'polygonGeoJsonGeometry',
-  'polygonCapColor',
-  'polygonCapMaterial',
-  'polygonSideColor',
-  'polygonSideMaterial',
-  'polygonStrokeColor',
-  'polygonAltitude',
-  'polygonCapCurvatureResolution',
-  'polygonsTransitionDuration'
-].map(p => ({ [p]: bindPolygonsLayer.linkProp(p)})));
+const linkedPolygonsLayerProps = Object.assign(
+  ...[
+    'polygonsData',
+    'polygonGeoJsonGeometry',
+    'polygonCapColor',
+    'polygonCapMaterial',
+    'polygonSideColor',
+    'polygonSideMaterial',
+    'polygonStrokeColor',
+    'polygonAltitude',
+    'polygonCapCurvatureResolution',
+    'polygonsTransitionDuration',
+  ].map((p) => ({ [p]: bindPolygonsLayer.linkProp(p) })),
+);
 
 const bindPathsLayer = linkKapsule('pathsLayer', PathsLayerKapsule);
-const linkedPathsLayerProps = Object.assign(...[
-  'pathsData',
-  'pathPoints',
-  'pathPointLat',
-  'pathPointLng',
-  'pathPointAlt',
-  'pathResolution',
-  'pathColor',
-  'pathStroke',
-  'pathDashLength',
-  'pathDashGap',
-  'pathDashInitialGap',
-  'pathDashAnimateTime',
-  'pathTransitionDuration'
-].map(p => ({ [p]: bindPathsLayer.linkProp(p)})));
+const linkedPathsLayerProps = Object.assign(
+  ...[
+    'pathsData',
+    'pathPoints',
+    'pathPointLat',
+    'pathPointLng',
+    'pathPointAlt',
+    'pathResolution',
+    'pathColor',
+    'pathStroke',
+    'pathDashLength',
+    'pathDashGap',
+    'pathDashInitialGap',
+    'pathDashAnimateTime',
+    'pathTransitionDuration',
+  ].map((p) => ({ [p]: bindPathsLayer.linkProp(p) })),
+);
 
 const bindTilesLayer = linkKapsule('tilesLayer', TilesLayerKapsule);
-const linkedTilesLayerProps = Object.assign(...[
-  'tilesData',
-  'tileLat',
-  'tileLng',
-  'tileAltitude',
-  'tileWidth',
-  'tileHeight',
-  'tileUseGlobeProjection',
-  'tileMaterial',
-  'tileCurvatureResolution',
-  'tilesTransitionDuration'
-].map(p => ({ [p]: bindTilesLayer.linkProp(p)})));
+const linkedTilesLayerProps = Object.assign(
+  ...[
+    'tilesData',
+    'tileLat',
+    'tileLng',
+    'tileAltitude',
+    'tileWidth',
+    'tileHeight',
+    'tileUseGlobeProjection',
+    'tileMaterial',
+    'tileCurvatureResolution',
+    'tilesTransitionDuration',
+  ].map((p) => ({ [p]: bindTilesLayer.linkProp(p) })),
+);
 
-const bindLabelsLayer = linkKapsule('labelsLayer', LabelsLayerKapsule);
-const linkedLabelsLayerProps = Object.assign(...[
-  'labelsData',
-  'labelLat',
-  'labelLng',
-  'labelAltitude',
-  'labelRotation',
-  'labelText',
-  'labelSize',
-  'labelTypeFace',
-  'labelColor',
-  'labelResolution',
-  'labelIncludeDot',
-  'labelDotRadius',
-  'labelDotOrientation',
-  'labelsTransitionDuration'
-].map(p => ({ [p]: bindLabelsLayer.linkProp(p)})));
+// const bindLabelsLayer = linkKapsule('labelsLayer', LabelsLayerKapsule);
+// const linkedLabelsLayerProps = Object.assign(
+//   ...[
+//     'labelsData',
+//     'labelLat',
+//     'labelLng',
+//     'labelAltitude',
+//     'labelRotation',
+//     'labelText',
+//     'labelSize',
+//     'labelTypeFace',
+//     'labelColor',
+//     'labelResolution',
+//     'labelIncludeDot',
+//     'labelDotRadius',
+//     'labelDotOrientation',
+//     'labelsTransitionDuration',
+//   ].map((p) => ({ [p]: bindLabelsLayer.linkProp(p) })),
+// );
 
 const bindRingsLayer = linkKapsule('ringsLayer', RingsLayerKapsule);
-const linkedRingsLayerProps = Object.assign(...[
-  'ringsData',
-  'ringLat',
-  'ringLng',
-  'ringAltitude',
-  'ringColor',
-  'ringResolution',
-  'ringMaxRadius',
-  'ringPropagationSpeed',
-  'ringRepeatPeriod'
-].map(p => ({ [p]: bindRingsLayer.linkProp(p)})));
+const linkedRingsLayerProps = Object.assign(
+  ...[
+    'ringsData',
+    'ringLat',
+    'ringLng',
+    'ringAltitude',
+    'ringColor',
+    'ringResolution',
+    'ringMaxRadius',
+    'ringPropagationSpeed',
+    'ringRepeatPeriod',
+  ].map((p) => ({ [p]: bindRingsLayer.linkProp(p) })),
+);
 
 const bindHtmlElementsLayer = linkKapsule('htmlElementsLayer', HtmlElementsLayerKapsule);
-const linkedHtmlElementsLayerProps = Object.assign(...[
-  'htmlElementsData',
-  'htmlLat',
-  'htmlLng',
-  'htmlAltitude',
-  'htmlElement',
-  'htmlTransitionDuration'
-].map(p => ({ [p]: bindHtmlElementsLayer.linkProp(p)})));
+const linkedHtmlElementsLayerProps = Object.assign(
+  ...['htmlElementsData', 'htmlLat', 'htmlLng', 'htmlAltitude', 'htmlElement', 'htmlTransitionDuration'].map((p) => ({
+    [p]: bindHtmlElementsLayer.linkProp(p),
+  })),
+);
 
 const bindObjectsLayer = linkKapsule('objectsLayer', ObjectsLayerKapsule);
-const linkedObjectsLayerProps = Object.assign(...[
-  'objectsData',
-  'objectLat',
-  'objectLng',
-  'objectAltitude',
-  'objectThreeObject'
-].map(p => ({ [p]: bindObjectsLayer.linkProp(p)})));
+const linkedObjectsLayerProps = Object.assign(
+  ...['objectsData', 'objectLat', 'objectLng', 'objectAltitude', 'objectThreeObject'].map((p) => ({
+    [p]: bindObjectsLayer.linkProp(p),
+  })),
+);
 
 const bindCustomLayer = linkKapsule('customLayer', CustomLayerKapsule);
-const linkedCustomLayerProps = Object.assign(...[
-  'customLayerData',
-  'customThreeObject',
-  'customThreeObjectUpdate'
-].map(p => ({ [p]: bindCustomLayer.linkProp(p)})));
+const linkedCustomLayerProps = Object.assign(
+  ...['customLayerData', 'customThreeObject', 'customThreeObjectUpdate'].map((p) => ({
+    [p]: bindCustomLayer.linkProp(p),
+  })),
+);
 
 //
 
@@ -240,7 +249,7 @@ export default Kapsule({
       onChange(rendererSize, state) {
         state.pathsLayer.rendererSize(rendererSize);
       },
-      triggerUpdate: false
+      triggerUpdate: false,
     },
     ...linkedGlobeLayerProps,
     ...linkedPointsLayerProps,
@@ -250,11 +259,11 @@ export default Kapsule({
     ...linkedHexedPolygonsLayerProps,
     ...linkedPathsLayerProps,
     ...linkedTilesLayerProps,
-    ...linkedLabelsLayerProps,
+    // ...linkedLabelsLayerProps,
     ...linkedRingsLayerProps,
     ...linkedHtmlElementsLayerProps,
     ...linkedObjectsLayerProps,
-    ...linkedCustomLayerProps
+    ...linkedCustomLayerProps,
   },
 
   methods: {
@@ -268,7 +277,7 @@ export default Kapsule({
         const pov = globePos ? globalPov.clone().sub(globePos) : globalPov; // convert to local vector
 
         let povDist, povEdgeDist, povEdgeAngle, maxSurfacePosAngle;
-        isBehindGlobe = pos => {
+        isBehindGlobe = (pos) => {
           povDist === undefined && (povDist = pov.length());
 
           // check if it's behind plane of globe's visible area
@@ -276,21 +285,21 @@ export default Kapsule({
           // return pov.angleTo(pos) > maxSurfacePosAngle;
 
           // more sophisticated method that checks also pos altitude
-          povEdgeDist === undefined && (povEdgeDist = Math.sqrt(povDist**2 - globeRadius**2));
+          povEdgeDist === undefined && (povEdgeDist = Math.sqrt(povDist ** 2 - globeRadius ** 2));
           povEdgeAngle === undefined && (povEdgeAngle = Math.acos(povEdgeDist / povDist));
           const povPosDist = pov.distanceTo(pos);
           if (povPosDist < povEdgeDist) return false; // pos is closer than visible edge of globe
 
           const posDist = pos.length();
-          const povPosAngle = Math.acos((povDist**2 + povPosDist**2 - posDist**2) / (2 * povDist * povPosDist)); // triangle solver
+          const povPosAngle = Math.acos((povDist ** 2 + povPosDist ** 2 - posDist ** 2) / (2 * povDist * povPosDist)); // triangle solver
           return povPosAngle < povEdgeAngle; // pos is within globe's visible area cone
         };
       }
 
       // pass behind globe checker for layers that need it
-      state.layersThatNeedBehindGlobeChecker.forEach(l => l.isBehindGlobe(isBehindGlobe));
+      state.layersThatNeedBehindGlobeChecker.forEach((l) => l.isBehindGlobe(isBehindGlobe));
     },
-    ...linkedGlobeLayerMethods
+    ...linkedGlobeLayerMethods,
   },
 
   stateInit: () => {
@@ -303,16 +312,16 @@ export default Kapsule({
       hexedPolygonsLayer: HexedPolygonsLayerKapsule(),
       pathsLayer: PathsLayerKapsule(),
       tilesLayer: TilesLayerKapsule(),
-      labelsLayer: LabelsLayerKapsule(),
+      // labelsLayer: LabelsLayerKapsule(),
       ringsLayer: RingsLayerKapsule(),
       htmlElementsLayer: HtmlElementsLayerKapsule(),
       objectsLayer: ObjectsLayerKapsule(),
-      customLayer: CustomLayerKapsule()
+      customLayer: CustomLayerKapsule(),
     };
 
     return {
       ...layers,
-      layersThatNeedBehindGlobeChecker: Object.values(layers).filter(l => l.hasOwnProperty('isBehindGlobe'))
+      layersThatNeedBehindGlobeChecker: Object.values(layers).filter((l) => l.hasOwnProperty('isBehindGlobe')),
     };
   },
 
@@ -321,11 +330,11 @@ export default Kapsule({
     emptyObject(threeObj);
 
     // Main three object to manipulate
-    threeObj.add(state.scene = new THREE.Group());
+    threeObj.add((state.scene = new THREE.Group()));
     state.scene.visible = false; // hide scene before globe initialization
 
     // Add all layers groups
-    layers.forEach(layer => {
+    layers.forEach((layer) => {
       const g = new THREE.Group();
       state.scene.add(g);
       state[layer](g);
@@ -336,17 +345,17 @@ export default Kapsule({
         // Animate build-in just once
         state.scene.scale.set(1e-6, 1e-6, 1e-6);
 
-        new TWEEN.Tween({k: 1e-6})
-          .to({k: 1}, 600)
+        new TWEEN.Tween({ k: 1e-6 })
+          .to({ k: 1 }, 600)
           .easing(TWEEN.Easing.Quadratic.Out)
-          .onUpdate(({k}) => state.scene.scale.set(k, k, k))
+          .onUpdate(({ k }) => state.scene.scale.set(k, k, k))
           .start();
 
         const rotAxis = new THREE.Vector3(0, 1, 0);
-        new TWEEN.Tween({rot: Math.PI * 2})
-          .to({rot: 0}, 1200)
+        new TWEEN.Tween({ rot: Math.PI * 2 })
+          .to({ rot: 0 }, 1200)
           .easing(TWEEN.Easing.Quintic.Out)
-          .onUpdate(({rot}) => state.scene.setRotationFromAxisAngle(rotAxis, rot))
+          .onUpdate(({ rot }) => state.scene.setRotationFromAxisAngle(rotAxis, rot))
           .start();
       }
 
@@ -354,9 +363,7 @@ export default Kapsule({
       state.onGlobeReady && state.onGlobeReady();
     };
 
-    waitForGlobeReady
-      ? state.globeLayer.onReady(initGlobe)
-      : initGlobe();
+    waitForGlobeReady ? state.globeLayer.onReady(initGlobe) : initGlobe();
 
     // run tween updates
     (function onFrame() {
@@ -365,5 +372,5 @@ export default Kapsule({
     })(); // IIFE
   },
 
-  update(state) {}
+  update(state) {},
 });
